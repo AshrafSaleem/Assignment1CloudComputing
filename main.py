59abbabac4a034b19296b56ae10b216f7ed357de
AshrafSaleem <30308340+AshrafSaleem@users.noreply.github.com>
2021-04-16 10:38:55 +1000

    Web service Object Detection Integretion.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import json
+from object_detection import object_detection
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 @app.route('/api/object_detection', methods=["POST"])
-def object_detection():
-    client_data = json.loads(request.data)
-    return jsonify(client_data)
+def object_detection_server():
+    client_data = json.loads(json.loads(request.data))
+    result = object_detection(client_data["image"])
+    return jsonify({"id": client_data["id"], "objects": result})
 
-app.run(debug = True)
+app.run(debug = True, threaded = True)
